66df2347aa5ca08f2aa2d072acab85c706b8f346
mitsbits <mitsbits@gmail.com>
2019-02-28 23:18:06 +0200

    start cache wrapper tests

---
+++ test/Test.Borg.Framework.MVC/Cache/InMemoryDistributedCacheWrapperTests.cs
@@ -0,0 +1,18 @@
+﻿using Borg.Framework.MVC.Cache;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Test.Borg.Framework.MVC.Cache
+{
+    public class InMemoryDistributedCacheWrapperTests
+    {
+        private readonly IMemoryCache _memoryCache;
+        private readonly IDistributedCache _cache;
+
+        public InMemoryDistributedCacheWrapperTests()
+        {
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+            _cache = new InMemoryDistributedCacheWrapper(_memoryCache);
+        }
+    }
+}
