225f655ab14a1731a3781b0129c8e81b412589db
mitsbits <mitsbits@gmail.com>
2019-04-02 23:34:44 +0300

    .

---
+++ src/System/Borg.System.Backoffice.Security/SecurityHeadersPolicy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Borg.System.Backoffice.Security
+{
+    public class SecurityHeadersPolicy
+    {
+        public IDictionary<string, string> SetHeaders { get; }
+             = new Dictionary<string, string>();
+
+        public ISet<string> RemoveHeaders { get; }
+            = new HashSet<string>();
+    }
+}
